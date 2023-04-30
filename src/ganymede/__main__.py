d2eab69391e9a5d12f80ed192e1e53312b40faec
Steven Carter <stevenandrewcarter@users.noreply.github.com>
2023-04-30 10:39:05 +0000

    Added some documentation

--- src/ganymede/__main__.py
+++ src/ganymede/__main__.py
@@ -1,10 +1,16 @@
-import click
+"""
+Ganymede is a tool that will convert existing Jupyter notebooks into a productionable API so that 
+the models defined can be used as part of an API call stack instead of being a workbook. The tool 
+provides a few options on how to convert the workbook, either making the entire solution API driven
+or just extracting code blocks from a workbook and allowing authors to import the source instead.
+"""
 import os
 import json
+import click
 
 if __name__ == "__main__":
     print(os.getcwd())
-    with open("./samples/test.ipynb") as f:
+    with open("./samples/test.ipynb", encoding='utf8') as f:
         data = json.load(f)
 
         print("Type:", type(data))
